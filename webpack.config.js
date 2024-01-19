8faa4f775159e405561f47d3be952e9e92606905
CosminManu <cosmin.manu2000@gmail.com>
2024-01-19 19:45:24 +0200

    finished wepack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const path = require('path')
 
 module.exports = {
-    
+    mode: 'development',
+    entry: './src/index.js',        //where we want webapck initially look for our js file
+    output: {
+        path: path.resolve(__dirname, 'dist'),     // __dirname = gets current directory
+        filename: 'bundle.js'
+    },
+    watch: true
 }
+
+//webapack will take the src file and any imports
+//will bundle them together and put them in dist/bundle.js
+//will watch at every change 
